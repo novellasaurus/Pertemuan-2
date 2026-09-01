f80420411cd5722cf5baf9c1e98e1ca1686cf8ab
novellasaurus <novelaja111@gmail.com>
2026-09-01 22:19:32 +0700

    Add files via upload

---
+++ tugas 2.py
@@ -0,0 +1,35 @@
+#1
+data_string =  "Muhammad Novel Nur Fahmi"
+print ("\nNama : ",data_string)
+data_integer = "19 Tahun"
+print ("\nUmur : ",data_integer)
+data_float = "65.5"
+print ("\nBerat Badan: ",data_float)
+#2 
+data_str = "123"
+data_int = int (data_str)
+print(" data - ", data_int,"type - ", type(data_int))
+
+data_float = 45.67
+data_int = int(data_float)
+print(" data - ", data_int,"type - ", type(data_int))
+
+data_int = 89
+data_float = float(data_int)
+print(" data - ", data_float, "type = ", type(data_float))
+
+data_int = 90
+data_str = str (data_int)
+print(" data - ", data_str, "type = ", type(data_str))
+#3
+
+angka = int(input("Masukkan Usia: "))
+print("data  ", angka, ",type =", type(angka))
+
+angka = float(input("Masukkan Tinggi Badan: "))
+print("data  ", angka, ",type =",type(angka))
+
+data = input("Masukkan Nama: ")
+print("data ", data,",type =", type(data))
+
+
